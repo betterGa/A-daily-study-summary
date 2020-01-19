a7f1a37973e8fdc053689f4d185dc5d651fdeb2e
yourjia <2086543608@qq.com>
2020-01-19 15:53:07 +0800

    20200119

---
+++ src/main/java/B20200119.java
@@ -0,0 +1,20 @@
+import jdk.nashorn.internal.ir.BaseNode;
+
+ class Person
+{
+    private String name="Person";
+    int age=0;}
+
+public class B20200119 extends Person {
+  public String grade;
+
+    public static void main(String[] args) {
+        Person p=new B20200119();
+        System.out.println(p.name);
+        }
+    }
+
+
+
+
+
